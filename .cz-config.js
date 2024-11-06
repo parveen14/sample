5e78c9eacb1a09eb820b4a83dfb2a7db17bf5e3d
parveen14 <parveen.kngr@gmail.com>
2024-11-06 12:55:36 +0530

    feat(ci-cd):

--- .cz-config.js
+++ .cz-config.js
@@ -52,8 +52,7 @@ module.exports = {
     body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
     breaking: "List any BREAKING CHANGES (optional):\n",
     footer: "Time spent on this ticket (in hours)",
-    footer:
-      "List any ISSUES CLOSED by this change (optional). E.g.: RPMS-3551, RPMS-34:\n",
+
     confirmCommit: "Are you sure you want to proceed with the commit above?",
   },
 
