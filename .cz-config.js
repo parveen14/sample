044206d603f2dce53b95ca222c18e87525323b5d
parveen14 <parveen.kngr@gmail.com>
2024-11-06 11:43:12 +0530

    feat(ci-cd):

--- .cz-config.js
+++ .cz-config.js
@@ -51,10 +51,7 @@ module.exports = {
     subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
     body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
     breaking: "List any BREAKING CHANGES (optional):\n",
-    ticket_time: {
-      description: "Enter ticket time (optional). E.g 2h 30m:",
-      type: "input",
-    },
+    ticket_time: "Enter ticket time (optional). E.g 2h 30m:",
     footer:
       "List any ISSUES CLOSED by this change (optional). E.g.: RPMS-31dd, RPMS-34:\n",
     confirmCommit: "Are you sure you want to proceed with the commit above?",
