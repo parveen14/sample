351bfbe63da9a4ed7f3f4d3399ab8032bbe86fcc
parveen14 <parveen.kngr@gmail.com>
2024-11-06 12:30:14 +0530

    feat(ci-cd):

--- .cz-config.js
+++ .cz-config.js
@@ -42,20 +42,6 @@ module.exports = {
   isTicketNumberRequired: false,
   ticketNumberPrefix: "Fixes - ",
 
-  // override the messages, defaults are as follows
-  messages: {
-    type: "Select the type of change that you're committing:",
-    scope: "Denote the SCOPE of this change:",
-    // used if allowCustomScopes is true
-    customScope: "Denote the SCOPE of this change:",
-    subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
-    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-    breaking: "List any BREAKING CHANGES (optional):\n",
-    footer:
-      "List any ISSUES CLOSED by this change (optional). E.g.: RPMS-31, RPMS-34:\n",
-    confirmCommit: "Are you sure you want to proceed with the commit above?",
-  },
-
   questions: [
     {
       type: "input",
