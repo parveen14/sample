0194e2dac7bd5958cd38159e833ac26ac999f412
parveen14 <parveen.kngr@gmail.com>
2024-12-08 11:14:53 +0530

    fix(migrations): testing this
    there is no longer description
    
    here
    #time 1h 20m
    #comment No comment
    
    BREAKING CHANGE:
    hello

--- .cz-config.js
+++ .cz-config.js
@@ -55,6 +55,21 @@ module.exports = {
       "List any ISSUES CLOSED by this change (optional). E.g.: RPMS-31, RPMS-34:\n",
     confirmCommit: "Are you sure you want to proceed with the commit above?",
   },
+
+  additionalQuestions: [
+    {
+      type: "input",
+      name: "time",
+      message: "Time spent (i.e. 1h 15m) (optional):\n",
+      mapping: "#time",
+    },
+    {
+      type: "input",
+      name: "comment",
+      message: "Jira comment (optional):\n",
+      mapping: "#comment",
+    },
+  ],
   allowCustomScopes: false,
   allowBreakingChanges: ["feat", "fix"],
 
